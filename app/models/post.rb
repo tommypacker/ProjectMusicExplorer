42de2d5b3910c3f9028784ed196221e0774fc121
tommypacker <tommypacker@gmail.com>
2016-02-29 18:57:12 -0600

    Make sure the post can't have any blank fields

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 	acts_as_votable
+	validates :title, :presence => true, :uniqueness => true, :length => { :maximum => 50 }
+	validates :url, :presence => true, :uniqueness => true
+	validates :description, :presence => true, :uniqueness => true, :length => { :maximum => 2000 }
 end
